7c2e22055bcf272b280518b0d21812605976c229
gregory <gcvalderrama@hotmail.com>
2020-08-11 13:07:51 -0500

    add

--- docker-run.sh
+++ docker-run.sh
@@ -6,5 +6,5 @@ docker pull apankura/java-spring-gradle-one-db2-spring
 
 docker run -itd --rm --name db2 --privileged=true -p 50000:50000 -e LICENSE=accept -e DB2INST1_PASSWORD=admin123 -e DBNAME=sutdb apankura/java-spring-gradle-one-db2-db2
 sleep 10
-docker run -itd --rm --name spring --privileged=true -p 46801:46801 -e SPRING_PROFILES_ACTIVE=docker -e DB2_HOST=192.168.0.14 apankura/java-spring-gradle-one-db2-spring
+docker run -itd --rm --name spring -p 46801:46801 -e SPRING_PROFILES_ACTIVE=docker -e DB2_HOST=192.168.0.14 apankura/java-spring-gradle-one-db2-spring
 sleep 10
